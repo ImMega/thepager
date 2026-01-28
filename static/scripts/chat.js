31bd283af75798d12264e3ae0a5e5ce83863f342
ImMega <72713009+ImMega@users.noreply.github.com>
2026-01-28 09:59:49 +0100

    Change chat test to main

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,4 +1,9 @@
 var username;
+const socket = io();
+var chatid;
+var newdmgroup = 0;
+var newchatusers = [];
+
 
 window.onload = fetch('/get-user').then((res) => {
     res.json().then(async (data) => {
@@ -8,20 +13,54 @@ window.onload = fetch('/get-user').then((res) => {
 
         document.getElementById("greeting").innerHTML = "Hi, " + data.username + "!";
 
-        const chatRes = await fetch('/get-chat', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ chatId: "1234567890" })
-        });
-        const chatData = await chatRes.json();
+        chatid = document.location.pathname.split("/")[2];
+
+        if (chatid == "global-chat") document.getElementById("global-chat").classList.add("room-active");
+
+        const roomsRes = await fetch('/get-rooms', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ username: username }) });
+        const roomsJson = await roomsRes.json();
+        const roomsData = roomsJson.data;
+        const rooms = document.createDocumentFragment();
+
+        if (!roomsData.length) {
+            const noChatsElement = document.createElement("p");
+            noChatsElement.classList.add("no-chats-text")
+            noChatsElement.innerText = "You have no chats.\n You can try creating some!";
+
+            rooms.appendChild(noChatsElement);
+        }
+
+        if (roomsData) for (var i = 0; i < roomsData.length; i++) {
+            const thisRoom = roomsData[i];
+            const roomElement = roomElCreate(thisRoom.id, thisRoom.name, thisRoom.isDm, (thisRoom.lastSeen < thisRoom.lastMsgTimestamp && thisRoom.id != chatid));
+
+            rooms.appendChild(roomElement);
+        }
+
+        document.getElementById("room-list").append(rooms);
+
+        if (roomsData) for (var i = 0; i < roomsData.length; i++) {
+            document.getElementById(roomsData[i].id).addEventListener("click", function () { switchChat(this.id) });
+        }
+
+        if (!chatid) return;
+
+        const chatRes = await fetch('/get-chat', {method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ chatId: chatid }) });
+        const chatJson = await chatRes.json();
+
+        if (chatRes.status == 404) return document.location.href = "/chat";
+
+        document.title = "Pager - " + chatJson.name;
+
+        document.getElementById(chatid).scrollIntoView({ block: "center" });
+
+        socket.emit("chat-user-connected", chatid, username);
 
         const chatbox = document.getElementById("chatbox");
-        const msgsData = chatData.data.messages;
+        const msgsData = chatJson.data.messages;
         const msgs = document.createDocumentFragment();
 
-        for (let i = 0; i < msgsData.length; i++) {
+        for (var i = 0; i < msgsData.length; i++) {
             const thisMsg = msgsData[i];
 
             if (i > 0) {
@@ -49,16 +88,17 @@ window.onload = fetch('/get-user').then((res) => {
 
         chatbox.append(msgs);
 
-        chatbox.scrollTop = chatbox.scrollHeight;
+        setTimeout(() => {
+            chatbox.scrollTop = chatbox.scrollHeight;
+        }, 100);
 
-        document.getElementById("maincontainer").style.opacity = 1;
+        document.getElementById("chat-container").classList.remove("full-hidden");
     });
 });
 
-const socket = io();
-
 document.onkeydown = (e) => {
-    if(e.keyCode == 13 && !e.repeat) return sendMsg();
+    if (document.getElementById("new-chat-dialog").open) return;
+    if (e.keyCode == 13 && !e.repeat) return sendMsg();
 }
 
 socket.on("chat-message", msg => {
@@ -89,6 +129,44 @@ socket.on("chat-message", msg => {
 
         chatbox.scrollTop = chatbox.scrollHeight;
     }
+
+    if (chatid == "global-chat") return;
+
+    const oldNode = document.getElementById(chatid);
+
+    if (oldNode.isSameNode(document.getElementById("room-list").firstChild)) return;
+
+    const newNode = oldNode.cloneNode(true);
+
+    document.getElementById("room-list").prepend(newNode);
+
+    oldNode.remove();
+
+    document.getElementById(chatid).addEventListener("click", function () { switchChat(this.id) });
+});
+
+socket.on("direct-new-room-message", (chatId) => {
+    if (document.getElementById(chatId + "newmsg")) return;
+
+    const newMsgIndicator = document.createElement("div");
+    newMsgIndicator.id = chatId + "newmsg";
+    newMsgIndicator.classList.add("new-message-indicator");
+
+    document.getElementById(chatId).append(newMsgIndicator);
+
+    if (chatId == "global-chat") return;
+    
+    const oldNode = document.getElementById(chatId);
+
+    if (oldNode.isSameNode(document.getElementById("room-list").firstChild)) return;
+
+    const newNode = oldNode.cloneNode(true);
+
+    document.getElementById("room-list").prepend(newNode);
+
+    oldNode.remove();
+
+    document.getElementById(chatId).addEventListener("click", function () { switchChat(this.id) });
 });
 
 function msgCreate(message, author, themselves) {
@@ -109,16 +187,45 @@ function msgCreate(message, author, themselves) {
     return msgElement;
 }
 
+function roomElCreate(roomid, name, isDm, newMsg) {
+    const currentChatId = document.location.pathname.split("/")[2];
+
+    const roomElement = document.createElement("div");
+    const chatIcon = document.createElement("img");
+    const nameElement = document.createElement("div");
+    const newMsgElement = document.createElement("div");
+
+    roomElement.classList.add("room");
+    nameElement.classList.add("room-name");
+    chatIcon.src = isDm ? "/static/svg/direct_message_icon.svg" : "/static/svg/group_chat_icon.svg";
+    chatIcon.alt = isDm ? "Direct Message" : "Group Chat";
+    chatIcon.height = "20";
+    chatIcon.width = "20";
+    chatIcon.classList.add("dm-group-icon");
+    newMsgElement.classList.add("new-message-indicator");
+    if (roomid == currentChatId) roomElement.classList.add("room-active");
+
+    roomElement.id = roomid;
+    nameElement.innerText = name;
+
+    roomElement.append(chatIcon, nameElement);
+
+    if (newMsg) roomElement.append(newMsgElement);
+
+    return roomElement;
+}
+
 function sendMsg() {
     const chatbox = document.getElementById("chatbox");
     const msgInput = document.getElementById("chatinput");
 
     if (!msgInput.value) return;
 
-    socket.emit("send-message", msgInput.value);
+    const chatid = document.location.pathname.split("/")[2];
+
+    socket.emit("send-message", chatid, msgInput.value);
 
     if (chatbox.children.length > 0) {
-        console.log("aa")
         const lastMsg = chatbox.children.item(chatbox.children.length - 1);
 
         if (lastMsg.children.item(0).innerHTML == username) {
@@ -145,8 +252,138 @@ function sendMsg() {
     }
 
     msgInput.value = '';
+
+    if (chatid == "global-chat") return;
+    
+    const oldNode = document.getElementById(chatid);
+
+    if (oldNode.isSameNode(document.getElementById("room-list").firstChild)) return;
+
+    const newNode = oldNode.cloneNode(true);
+
+    document.getElementById("room-list").prepend(newNode);
+
+    oldNode.remove();
+
+    document.getElementById(chatid).addEventListener("click", function () { switchChat(this.id) });
 }
 
 function logout() {
     fetch("/logout").then(() => document.location.href = "/login");
+}
+
+function switchChat(chatid) {
+    document.location.href = "/chat/" + chatid;
+}
+
+// Chat creation functions
+function newChatUserCreate(user) {
+    const userElement = document.createElement("button");
+    userElement.classList.add("users-box-entry");
+    userElement.innerHTML = user + "&nbsp;&nbsp;X"
+
+    userElement.addEventListener("click", function () {
+        const index = newchatusers.indexOf(user);
+        if (index > -1) newchatusers.splice(index, 1);console.log(newchatusers)
+        if (!newdmgroup) document.getElementById("add-user-button").disabled = false;
+
+        this.remove();
+    });
+
+    return userElement;
+}
+
+function openNewChatDialog() {
+    newchatusers = [];
+    document.getElementById('new-chat-dialog').showModal();
+}
+
+function closeNewChatDialog() {
+    newchatusers = [];
+    document.getElementById("users-box").innerHTML = "";
+    document.getElementById("chat-name-input").value = "";
+    document.getElementById("add-user-button").disabled = false;
+    document.getElementById('new-chat-dialog').close();
+}
+
+function newChatSwitchDm() {
+    if (!newdmgroup) return;
+    document.getElementById("dm-button").classList.add("dm-chat-button-selected");
+    document.getElementById("chat-button").classList.remove("dm-chat-button-selected");
+
+    newdmgroup = 0;
+    newchatusers = [];
+    document.getElementById("users-box").innerHTML = "";
+    document.getElementById("chat-name-input").value = "";
+    document.getElementById("chat-name-text").hidden = true;
+    document.getElementById("chat-name-input-box").classList.add("full-hidden");
+}
+
+function newChatSwitchChat() {
+    if (newdmgroup) return;
+    document.getElementById("dm-button").classList.remove("dm-chat-button-selected");
+    document.getElementById("chat-button").classList.add("dm-chat-button-selected");
+
+    newdmgroup = 1;
+    newchatusers = [];
+    document.getElementById("users-box").innerHTML = "";
+    document.getElementById("chat-name-input").value = "";
+    document.getElementById("add-user-button").disabled = false;
+    document.getElementById("chat-name-text").hidden = false;
+    document.getElementById("chat-name-input-box").classList.remove("full-hidden");
+}
+
+function fireUserWarningText(message) {
+    const userWarningText = document.getElementById("user-warning-text");
+    userWarningText.innerText = message;
+    userWarningText.hidden = false;
+
+    setTimeout(() => {
+        userWarningText.hidden = true
+    }, 5000);
+}
+
+async function newChatAddUser() {
+    const userInput = document.getElementById("user-input");
+
+    if (newchatusers.includes(userInput.value)) return fireUserWarningText("User is already added!");
+
+    if (userInput.value == username) return fireUserWarningText("You can't add yourself. You'll be added automatically!");
+
+    const user = await fetch('/verify-user', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ username: userInput.value }) });
+    const userJson = await user.json();
+    const userExists = userJson.exists;
+
+    if (userExists) {
+        const userElement = newChatUserCreate(userInput.value);
+
+        newchatusers.push(userInput.value);console.log(newchatusers)
+        userInput.value = "";
+
+        document.getElementById("users-box").append(userElement);
+
+        if (!newdmgroup) document.getElementById("add-user-button").disabled = true;
+    } else {
+        fireUserWarningText("User does not exist!");
+    }
+}
+
+async function createNewChat() {
+    const chatName = document.getElementById("chat-name-input").value;
+
+    if (newdmgroup && !chatName) return fireUserWarningText("Group chat needs to have a name!");
+    if (!newchatusers.length) return fireUserWarningText("Can't create a chat with no users!");
+
+    const chatCreate = await fetch('/create-chat', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: chatName, users: newchatusers, isDm: !newdmgroup }) });
+    const chatCreateJson = await chatCreate.json();
+
+    if (chatCreateJson.success) {
+        const roomElement = roomElCreate(chatCreateJson.data.id, chatCreateJson.data.name, chatCreateJson.data.isDm, false);
+
+        document.getElementById("room-list").prepend(roomElement);
+
+        document.getElementById(chatCreateJson.data.id).addEventListener("click", function () { switchChat(this.id) });
+    }
+
+    closeNewChatDialog();
 }
